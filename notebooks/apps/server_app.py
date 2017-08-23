2694dae05da4163cafeac00bd32aa4ab758830fc
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2017-08-23 10:06:42 -0400

    Simplified server_app

--- notebooks/apps/server_app.py
+++ notebooks/apps/server_app.py
@@ -2,14 +2,13 @@ import dask.dataframe as dd
 import holoviews as hv
 from holoviews.operation.datashader import datashade
 
-# 1. Instead of using hv.extension, declare that we are using Bokeh and grab the renderer
-renderer = hv.renderer('bokeh')
+hv.extension('bokeh')
 
-# 2. Load data and datashade it
+# 1. Load data and Datashade it
 ddf = dd.read_parquet('../data/nyc_taxi_hours.parq/')[['dropoff_x', 'dropoff_y']].persist()
 points = hv.Points(ddf, kdims=['dropoff_x', 'dropoff_y'])
 shaded = datashade(points).opts(plot=dict(width=800, height=600))
 
-# 3. Instead of Jupyter's automatic rich display, render the object as a bokeh document
-doc = renderer.server_doc(shaded)
+# 2. Instead of Jupyter's automatic rich display, render the object as a bokeh document
+doc = hv.renderer('bokeh').server_doc(shaded)
 doc.title = 'HoloViews Bokeh App'
