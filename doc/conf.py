4db855403bb5f745b11319f576d5f14c2cb30158
C. E. Ball <ceball@gmail.com>
2017-11-28 19:58:39 +0000

    Renamed anacondaviz to pyviz, and added new logo.

--- doc/conf.py
+++ doc/conf.py
@@ -2,8 +2,8 @@
 
 from nbsite.shared_conf import *
 
-project = u'anacondaviz'
-authors = u'anacondaviz'
+project = u'pyviz'
+authors = u'pyviz'
 copyright = u'2017 ' + authors
 description = 'How to solve visualization problems with Python tools.'
 
@@ -14,9 +14,9 @@ release = '0.0.1'
 html_static_path += ['_static']
 html_theme = 'sphinx_ioam_theme'
 html_theme_options = {
-    'logo':'anacondaviz-logo.png',
+    'logo':'pyviz-logo.png',
     'favicon':'favicon.ico'
-#    'css':'anacondaviz.css'
+#    'css':'pyviz.css'
 }
 
 _NAV =  (
@@ -30,7 +30,7 @@ html_context.update({
     'DESCRIPTION': description,
     'AUTHOR': authors,
     # will work without this - for canonical (so can ignore when building locally or test deploying)    
-    'WEBSITE_SERVER': 'https://continuumio.github.io/anacondaviz',
+    'WEBSITE_SERVER': 'https://pyviz.github.io/pyviz',
     'VERSION': version,
     'NAV': _NAV,
     'LINKS': _NAV,
