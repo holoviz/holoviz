77d67cf383eb34b558c950b04861bd33d3892a4e
James A. Bednar <jbednar@continuum.io>
2018-02-19 13:57:06 -0600

    Initial version of pyviz module

---
+++ pyviz/__init__.py
@@ -0,0 +1,25 @@
+"""
+PyViz "metamodule" -- Convenience aliases and API imports for PyViz-related libraries.
+"""
+
+import holoviews as hv                                   # noqa (API import)
+import bokeh as bk                                       # noqa (API import)
+import numpy as np                                       # noqa (API import)
+import pandas as pd                                      # noqa (API import)
+import param as pm                                       # noqa (API import)
+import datashader as ds                                  # noqa (API import)
+import datashader as ds                                  # noqa (API import)
+import datashader.transfer_functions as tf               # noqa (API import)
+import holoviews.operation.datashader as hd              # noqa (API import)
+
+# Some libraries are loaded only if present; will raise error if used but not importable
+try:
+    import geoviews as gv                                # noqa (API import)
+except ImportError:
+    pass
+
+from holoviews import help                               # noqa (API import)
+
+__version__ = pm.Version(release=(1,9,2), fpath=__file__, commit="$Format:%h$", reponame='pyviz')
+
+
