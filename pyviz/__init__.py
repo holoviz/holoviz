2ca25747377c7dd67292dc5a316c586850a02f71
C. E. Ball <ceball@gmail.com>
2018-02-20 14:09:50 +0000

    Made version numbers consistent (don't know where 0.9.1 comes from, but we'll be updating it soon anyway).

--- pyviz/__init__.py
+++ pyviz/__init__.py
@@ -23,6 +23,6 @@ except ImportError:
 
 from holoviews import help                               # noqa (API import)
 
-__version__ = pm.Version(release=(1,9,2), fpath=__file__, commit="$Format:%h$", reponame='pyviz')
+__version__ = pm.Version(release=(0,9,1), fpath=__file__, commit="$Format:%h$", reponame='pyviz')
 
 
