beed2d5e6773661f74d9bf26b1a86df948824a90
James A. Bednar <jbednar@continuum.io>
2018-02-20 11:00:21 -0800

    Added dask.dataframe as dd

--- pyviz/__init__.py
+++ pyviz/__init__.py
@@ -13,6 +13,7 @@ import colorcet as cc                                    # noqa (API import)
 import datashader as ds                                  # noqa (API import)
 import datashader.transfer_functions as tf               # noqa (API import)
 import holoviews.operation.datashader as hd              # noqa (API import)
+import dask.dataframe as dd                              # noqa (API import)
 
 # Some libraries are loaded only if present; will raise error if used but not importable
 # Should consider making them fully lazily imported as well
