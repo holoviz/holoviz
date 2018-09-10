3cbf46247d9398478ccf8599f3dcbc7a5f4190fa
James A. Bednar <jbednar@continuum.io>
2018-09-10 10:25:22 -0500

    Separated functionality in taxi app, for responsiveness

--- apps/nyc_taxi/main.py
+++ apps/nyc_taxi/main.py
@@ -2,35 +2,41 @@ import holoviews as hv, geoviews as gv, param, parambokeh, dask.dataframe as dd,
 import panel as pp
 
 from colorcet import cm
-from holoviews.operation.datashader import datashade
+from holoviews.operation.datashader import rasterize, shade
 from holoviews.streams import RangeXY
 
 hv.extension('bokeh', logo=False)
 
 usecols = ['dropoff_x','dropoff_y','pickup_x','pickup_y','dropoff_hour','pickup_hour','passenger_count']
 df = dd.read_parquet('../data/nyc_taxi_wide.parq')[usecols].persist()
-
-url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{Z}/{Y}/{X}.jpg'
-tiles = gv.WMTS(url,crs=crs.GOOGLE_MERCATOR)
 opts = dict(width=1000,height=600,xaxis=None,yaxis=None,bgcolor='black',show_grid=False)
-max_pass = int(df.passenger_count.max().compute()+1)
+cmaps = ['fire','bgy','bgyw','bmy','gray','kbc']
 
-class NYCTaxiExplorer(hv.streams.Stream):
+
+class NYCTaxiExplorer(param.Parameterized):
     alpha      = param.Magnitude(default=0.75, doc="Alpha value for the map opacity")
-    colormap   = param.ObjectSelector(default='fire', objects=['fire','bgy','bgyw','bmy','gray','kbc'])
-    passengers = param.Range(default=(0,max_pass), bounds=(0,max_pass))
+    cmap       = param.ObjectSelector(cm['fire'], objects={c:cm[c] for c in cmaps})
+    hour       = param.Range(default=(0, 24), bounds=(0, 24))
     location   = param.ObjectSelector(default='dropoff', objects=['dropoff', 'pickup'])
 
-    @param.depends('alpha','colormap','passengers','location')
-    def view(self, **kwargs):
-        map_tiles = tiles.options(alpha=self.alpha, **opts)
-        points = hv.Points(df, [self.location+'_x', self.location+'_y'])
-        taxi_trips = datashade(points.select(passenger_count=self.passengers),
-            x_sampling=1, y_sampling=1, cmap=cm[self.colormap], width=1000, height=600)
-        return map_tiles * taxi_trips
+    @param.depends('location', 'hour')
+    def points(self):
+        points = hv.Points(df, kdims=[self.location+'_x', self.location+'_y'], vdims=['dropoff_hour'])
+        if self.hour != (0, 24): points = points.select(dropoff_hour=self.hour)
+        return points
+
+    @param.depends('alpha')
+    def tiles(self):
+        return gv.tile_sources.StamenTerrain.options(alpha=self.alpha, **opts)
+
+    def view(self):
+        points = hv.DynamicMap(self.points)
+        agg = rasterize(points, x_sampling=1, y_sampling=1, width=600, height=400)
+        stream = hv.streams.Params(self, ['cmap'])
+        tiles = hv.DynamicMap(self.tiles)
+        return tiles * shade(agg, streams=[stream])
 
 explorer = NYCTaxiExplorer(name="NYC Taxi Trips")
-r = pp.Row(explorer, explorer.view)
-r.server_doc()
 
-
+panel = pp.Row(explorer, explorer.view())
+panel.server_doc()
