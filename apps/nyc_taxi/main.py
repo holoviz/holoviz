63a39ffbccd313a580732b93f166772598b0badd
James A. Bednar <jbednar@continuum.io>
2018-09-10 07:39:42 -0500

    Moved to Panel

--- apps/nyc_taxi/main.py
+++ apps/nyc_taxi/main.py
@@ -1,6 +1,7 @@
 import holoviews as hv, geoviews as gv, param, parambokeh, dask.dataframe as dd, cartopy.crs as crs
+import panel as pp
 
-from colorcet import cm_n
+from colorcet import cm
 from holoviews.operation.datashader import datashade
 from holoviews.streams import RangeXY
 
@@ -16,22 +17,20 @@ max_pass = int(df.passenger_count.max().compute()+1)
 
 class NYCTaxiExplorer(hv.streams.Stream):
     alpha      = param.Magnitude(default=0.75, doc="Alpha value for the map opacity")
-    colormap   = param.ObjectSelector(default=cm_n["fire"], objects=cm_n.values())
-    hour       = param.Integer(default=None, bounds=(0, 23), doc="All hours by default; drag to select one hour")
+    colormap   = param.ObjectSelector(default='fire', objects=['fire','bgy','bgyw','bmy','gray','kbc'])
     passengers = param.Range(default=(0,max_pass), bounds=(0,max_pass))
     location   = param.ObjectSelector(default='dropoff', objects=['dropoff', 'pickup'])
 
-    def make_view(self, x_range, y_range, **kwargs):
+    @param.depends('alpha','colormap','passengers','location')
+    def view(self, **kwargs):
         map_tiles = tiles.options(alpha=self.alpha, **opts)
-        points = hv.Points(df, [self.location+'_x', self.location+'_y'], self.location+'_hour')
-        selection = {self.location+"_hour":self.hour if self.hour else (0,24), "passenger_count":self.passengers}
-        taxi_trips = datashade(points.select(**selection), x_sampling=1, y_sampling=1, cmap=self.colormap,
-                               dynamic=False, x_range=x_range, y_range=y_range, width=1000, height=600)
+        points = hv.Points(df, [self.location+'_x', self.location+'_y'])
+        taxi_trips = datashade(points.select(passenger_count=self.passengers),
+            x_sampling=1, y_sampling=1, cmap=cm[self.colormap], width=1000, height=600)
         return map_tiles * taxi_trips
 
 explorer = NYCTaxiExplorer(name="NYC Taxi Trips")
-dmap = hv.DynamicMap(explorer.make_view, streams=[explorer, RangeXY()])
+r = pp.Row(explorer, explorer.view)
+r.server_doc()
 
-plot = hv.renderer('bokeh').instance(mode='server').get_plot(dmap)
-parambokeh.Widgets(explorer, view_position='right', callback=explorer.event, plots=[plot.state],
-                   mode='server')
+
