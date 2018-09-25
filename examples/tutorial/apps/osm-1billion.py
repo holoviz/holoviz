ac1dfb82db5f6a36ae1dc95607b5f536cfddc009
Chris B <ceball@gmail.com>
2018-09-25 19:27:10 +0100

    Restore apps to running if imported, to remind myself about what goes wrong...

--- examples/tutorial/apps/osm-1billion.py
+++ examples/tutorial/apps/osm-1billion.py
@@ -5,33 +5,27 @@ from holoviews.operation.datashader import datashade
 from holoviews.streams import RangeXY
 from colorcet import cm_n
 
+hv.extension('bokeh', logo=False)
 
-if __name__ == "__main__":
-   hv.extension('bokeh', logo=False)
-   
-   try:
-       df = dd.read_parquet(os.path.join(os.path.dirname(__file__),'..','..','data','osm-1billion.snappy.parq')).persist()
-   except: # filenotfounderror or whatever it is
-       print("run download cmd for big datasets or whatever...")
-       raise
-   
-   url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{Z}/{Y}/{X}.jpg'
-   map_tiles = gv.WMTS(url,crs=crs.GOOGLE_MERCATOR)
-   
-   opts1 = dict(width=1000, height=600, xaxis=None, yaxis=None, bgcolor='black', show_grid=False)
-   opts2 = dict(width=1000, height=600, x_sampling=1, y_sampling=1, dynamic=False)
-   
-   class OSMExplorer(hv.streams.Stream):
-       alpha      = param.Magnitude(default=0.75, doc="Map opacity")
-       colormap   = param.ObjectSelector(default=cm_n["fire"], objects=cm_n.values())
-   
-       def make_view(self, x_range, y_range, **kwargs):
-           tiles  = map_tiles.options(alpha=self.alpha, **opts1)
-           points = hv.Points(df, ['x','y'])
-           return tiles * datashade(points, cmap=self.colormap, x_range=x_range, y_range=y_range, **opts2)
-   
-   explorer = OSMExplorer(name="Open Street Map GPS")
-   dmap = hv.DynamicMap(explorer.make_view, streams=[explorer, RangeXY()])
-   
-   plot = hv.renderer('bokeh').instance(mode='server').get_plot(dmap)
-   parambokeh.Widgets(explorer, view_position='right', callback=explorer.event, plots=[plot.state], mode='server')
+df = dd.read_parquet(os.path.join(os.path.dirname(__file__),'..','..','data','osm-1billion.snappy.parq')).persist()
+
+url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{Z}/{Y}/{X}.jpg'
+map_tiles = gv.WMTS(url,crs=crs.GOOGLE_MERCATOR)
+
+opts1 = dict(width=1000, height=600, xaxis=None, yaxis=None, bgcolor='black', show_grid=False)
+opts2 = dict(width=1000, height=600, x_sampling=1, y_sampling=1, dynamic=False)
+
+class OSMExplorer(hv.streams.Stream):
+    alpha      = param.Magnitude(default=0.75, doc="Map opacity")
+    colormap   = param.ObjectSelector(default=cm_n["fire"], objects=cm_n.values())
+
+    def make_view(self, x_range, y_range, **kwargs):
+        tiles  = map_tiles.options(alpha=self.alpha, **opts1)
+        points = hv.Points(df, ['x','y'])
+        return tiles * datashade(points, cmap=self.colormap, x_range=x_range, y_range=y_range, **opts2)
+
+explorer = OSMExplorer(name="Open Street Map GPS")
+dmap = hv.DynamicMap(explorer.make_view, streams=[explorer, RangeXY()])
+
+plot = hv.renderer('bokeh').instance(mode='server').get_plot(dmap)
+parambokeh.Widgets(explorer, view_position='right', callback=explorer.event, plots=[plot.state], mode='server')
