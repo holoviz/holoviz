b19f20ed08e91f5f5a3e4401b92af0519f991a50
James A. Bednar <jbednar@continuum.io>
2018-10-12 16:35:03 -0500

    Fixed changes to player app

--- examples/tutorial/apps/taxi_player/main.py
+++ examples/tutorial/apps/taxi_player/main.py
@@ -78,10 +78,10 @@ def animate():
         button.label = '\u23F8 Pause'
         curdoc().add_periodic_callback(animate_update, 500)
     else:
-        button.label = '\u25B6  Play'
+        button.label = '\u25B6 Play'
         curdoc().remove_periodic_callback(animate_update)
 
-button = Button(label='\u25B6  Play', width=60)
+button = Button(label='\u25B6 Play', width=60)
 button.on_click(animate)
 
 widget = parambokeh.Widgets(cmap_picker, mode='raw')
