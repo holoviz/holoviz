10b81f7aea12246be77e71af97419b576f9643f5
Julia Signell <jsignell@gmail.com>
2019-06-13 13:41:11 -0400

    Fixing conda build, only one set of tests (#183)

--- dodo.py
+++ dodo.py
@@ -14,7 +14,8 @@ def task_test_user_install_part2_conda():
         "pyviz examples --path=. --force --use-test-data",
         # TODO: bokeh sampledata isn't a documented step
         "bokeh sampledata",
-        'pytest --nbsmoke-run -k ".ipynb" --ignore=tutorials/apps']}
+        'pytest --nbsmoke-run -k ".ipynb"',
+        'pytest --nbsmoke-run -k "07_Geographic_Data.ipynb" --ignore-nbsmoke-skip-run']}
 
 def task_test_user_install_part2_pip():
     return {'actions':[
