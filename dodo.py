cc8aa37f63e5eed011d79779bf99002a916f8c82
Julia Signell <jsignell@gmail.com>
2019-06-19 10:12:21 -0400

    Top level rename

--- dodo.py
+++ dodo.py
@@ -5,13 +5,12 @@ if "PYCTDEV_ECOSYSTEM" not in os.environ:
 from pyctdev import *  # noqa: api
 
 def task_test_user_install_part1():
-    return {'actions':["conda create -y -n pyviz-tutorial python=3.6"]}
+    return {'actions':["conda create -y -n holoviz-tutorial python=3.6"]}
 
 def task_test_user_install_part2_conda():
     return {'actions':[
-        "conda install -y -c pyviz/label/dev pyviz",
-        "conda install -y -c pyviz nbsmoke",
-        "pyviz examples --path=. --force --use-test-data",
+        "conda install -y -c pyviz/label/dev holoviz nbsmoke",
+        "holoviz examples --path=. --force --use-test-data",
         # TODO: bokeh sampledata isn't a documented step
         "bokeh sampledata",
         'pytest --nbsmoke-run -k ".ipynb"',
@@ -19,8 +18,8 @@ def task_test_user_install_part2_conda():
 
 def task_test_user_install_part2_pip():
     return {'actions':[
-        "pip install pyviz nbsmoke",
-        "pyviz examples --path=. --force --use-test-data",
+        "pip install holoviz nbsmoke",
+        "holoviz examples --path=. --force --use-test-data",
         # TODO: bokeh sampledata isn't a documented step
         "bokeh sampledata",
         'pytest --nbsmoke-run -k ".ipynb" --ignore=tutorials/apps']}
