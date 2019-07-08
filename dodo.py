e2fd914a8df4d43c49c78f4c71f76510055f1274
Julia Signell <jsignell@gmail.com>
2019-07-07 22:08:14 -0400

    Removing conda-build test_install

--- dodo.py
+++ dodo.py
@@ -9,6 +9,7 @@ def task_test_user_install_part1():
 
 def task_test_user_install_part2_conda():
     return {'actions':[
+        "conda uninstall -y conda-build",
         "conda install -y -c pyviz holoviz",
         "pip install nbsmoke",
         "holoviz examples --path=. --force --use-test-data",
