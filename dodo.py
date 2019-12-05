f1192cd8ba50273908671647118da3b27ed7db14
Chris B <ceball@users.sf.net>
2019-12-05 19:33:56 +0000

    More guessing about snappy.

--- dodo.py
+++ dodo.py
@@ -17,6 +17,8 @@ def task_test_user_install_part2_conda():
 
 def task_test_user_install_part2_pip():
     return {'actions':[
+        # would need to be documented: "pip users will need to install snappy e.g. via their system packaging tool (how to do this varies between platforms - or use conda)"
+        "pip install python-snappy",
         "pip install holoviz nbsmoke",
         "holoviz examples --path=. --force --use-test-data",
         # TODO: bokeh sampledata isn't a documented step
