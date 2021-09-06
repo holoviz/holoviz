4571fe2693196a573937c92e592441a6c6512abb
Pamela Painter Wadhwa <pwadhwa@quansight.com>
2021-09-06 13:30:30 -0500

    make dashboard notebooks into python files

---
+++ examples/tutorial/06_Dashboards.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import panel as pn
+import holoviews as hv
+
+pn.extension('tabulator', template='material', sizing_mode='stretch_width')
+
+import hvplot.pandas # noqa
+
+df = pd.read_parquet('../data/earthquakes-projected.parq')
+df = df.set_index('time').tz_convert(None).reset_index()
+
+from holoviews.element.tiles import WEB_MERCATOR_LIMITS
+
+subset_df = df[
+    (df['northing']  < WEB_MERCATOR_LIMITS[1]) &
+    (df['mag']       > 4) &
+    (df['time']     >= pd.Timestamp('2017-01-01')) &
+    (df['time']     <= pd.Timestamp('2018-01-01'))]
+
+date_subrange = pn.widgets.DateRangeSlider(name='Date', 
+                                        start=subset_df.time.iloc[0], 
+                                        end=subset_df.time.iloc[-1])
+mag_subrange = pn.widgets.FloatSlider(name='Magnitude', start=3, end=9, value=3)
+
+subset_dfi = subset_df.interactive(sizing_mode='stretch_width')
+filtered_subrange = subset_dfi[
+    (subset_dfi['mag']   > mag_subrange) &
+    (subset_dfi['time'] >= date_subrange.param.value_start) &
+    (subset_dfi['time'] <= date_subrange.param.value_end)]
+
+
+
+pn.state.template.sidebar_width = 250
+pn.state.template.title = 'Earthquake Interactive Demo'
+
+ls = hv.link_selections.instance(unselected_alpha=0.02)
+
+# Table is not yet dynamically linked to the linked selection
+table = filtered_subrange.pipe(ls.filter, selection_expr=ls.param.selection_expr)[['time', 'place', 'mag', 'depth']].pipe(
+    pn.widgets.Tabulator, pagination='remote', page_size=15)
+
+mag_hist = filtered_subrange.hvplot(
+    y='mag', kind='hist', responsive=True, min_height=300)
+
+depth_hist = filtered_subrange.hvplot(
+    y='depth', kind='hist', responsive=True, min_height=300)
+
+geo = filtered_subrange.hvplot(
+    'easting', 'northing', color='mag', kind='points',
+    xaxis=None, yaxis=None, responsive=True, min_height=500,
+    data_aspect=1, framewise=True, clim=(4, 10), line_color='black'
+)
+
+column = pn.Column(
+    pn.Row(
+        hv.element.tiles.ESRI() * ls(geo.holoviews()),
+        table.panel()
+    ),
+    pn.Row(
+        ls(depth_hist.holoviews()),
+        ls(mag_hist.holoviews()),
+    )
+)
+
+filtered_subrange.widgets().servable(area='sidebar')
+
+column.servable(title='Earthquake Interactive Demo')
---
+++ examples/tutorial/07_Custom_Interactivity.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import hvplot.pandas  # noqa
+from holoviews.element import tiles
+
+from holoviews import streams
+
+import holoviews as hv
+hv.extension("bokeh")
+
+import panel as pn
+
+
+df = pd.read_parquet('../data/earthquakes-projected.parq')
+df.time = df.time.astype('datetime64[ns]')
+df = df.set_index(df.time)
+
+most_severe = df[df.mag >= 7]
+high_mag_quakes = most_severe.hvplot.points(x='easting', y='northing', c='mag', 
+                                                      title='Earthquakes with magnitude >= 7')
+esri = tiles.ESRI().redim(x='easting', y='northing')
+
+
+
+selection_stream = streams.Selection1D(source=high_mag_quakes)
+
+hv.opts.defaults(hv.opts.Points(nonselection_alpha=0.4))
+
+def labelled_callback(index):
+    if len(index) == 0:
+        return  hv.Text(x=0,y=0, text='')
+    first_index = index[0] # Pick only the first one if multiple are selected
+    row = most_severe.iloc[first_index]
+    text = '%d : %s' % (first_index, row.place)
+    return hv.Text(x=row.easting, y=row.northing, text=text).opts(color='white')
+
+labeller = hv.DynamicMap(labelled_callback, streams=[selection_stream])
+
+def mark_earthquake(index):
+    if len(index) == 0:
+        return  hv.Overlay([])
+    first_index = index[0] # Pick only the first one if multiple are selected
+    row = most_severe.iloc[first_index]
+    return (hv.Ellipse(row.easting, row.northing, 1.5e6) *
+            hv.Ellipse(row.easting, row.northing, 3e6)).opts(
+        hv.opts.Ellipse(color='white', alpha=0.5)
+    )
+
+quake_marker = hv.DynamicMap(mark_earthquake, streams=[selection_stream])
+
+
+def earthquakes_around_point(df, lat, lon, degrees_dist=0.5):
+    half_dist = degrees_dist / 2.0
+    return df[((df['latitude'] - lat).abs() < half_dist) 
+              & ((df['longitude'] - lon).abs() < half_dist)]
+
+def index_to_selection(indices, cache={}):
+    if not indices: 
+        return most_severe.iloc[[]]
+    index = indices[0]   # Pick only the first one if multiple are selected
+    if index in cache: return cache[index]
+    row = most_severe.iloc[index]
+    selected_df = earthquakes_around_point(df, row.latitude, row.longitude)
+    cache[index] = selected_df
+    return selected_df 
+
+
+def histogram_callback(index):
+    title = 'Distribution of all magnitudes within half a degree of selection'
+    selected_df = index_to_selection(index)
+    return selected_df.hvplot.hist(y='mag', bin_range=(0,10), bins=20, color='red', title=title)
+
+histogram = hv.DynamicMap(histogram_callback, streams=[selection_stream])
+
+def scatter_callback(index):
+    title = 'Temporal distribution of all magnitudes within half a degree of selection '
+    selected_df = index_to_selection(index)
+    return selected_df.hvplot.scatter('time', 'mag', color='green', title=title)
+
+temporal_distribution = hv.DynamicMap(scatter_callback, streams=[selection_stream])
+
+def vline_callback(index):
+    if not index:
+        return hv.VLine(0).opts(alpha=0)
+    row = most_severe.iloc[index[0]]
+    return hv.VLine(row.time).opts(line_width=2, color='black')
+
+temporal_vline = hv.DynamicMap(vline_callback, streams=[selection_stream])
+
+column = pn.Column(((esri * high_mag_quakes.opts(tools=['tap']) * labeller * quake_marker)
+ + histogram + temporal_distribution * temporal_vline).cols(1))
+
+column.servable(title='Custom Interactivity Demo')
+
