5a44bc2bc562a0d56edfc7d1d8c7c05e2eb47d49
James A. Bednar <jbednar@continuum.io>
2024-07-05 20:36:41 -0500

    Point to data in /shared when running on Nebari

--- examples/tutorial/data_checker.py
+++ examples/tutorial/data_checker.py
@@ -1,5 +1,6 @@
 import pathlib
 import pandas as pd
+import os
 
 def check_data(file_path='../data/earthquakes-projected.parq'):
     """
@@ -10,7 +11,19 @@ def check_data(file_path='../data/earthquakes-projected.parq'):
 
     """
     path = pathlib.Path(file_path)
-    
+
+    # Hack to make Nebari installation work for SciPy 2024; can be deleted after the conference
+    try:
+        source = "/shared/scipy/hvplot-and-panel"
+        dest = "../data"
+        if os.path.exists(source) and not os.path.exists(dest):
+            print(f"Adding symlink to data files: {str(dest)} -> {str(source)}")
+            os.symlink(source, dest)
+
+    except RuntimeError as e:
+        print(f"Encountered exception during SciPy/Nebari setup: {str(e)}")
+
+    # Try to read the file
     if not path.is_file():
         print(f"Data file does not exist at {file_path}")
 
@@ -18,6 +31,6 @@ def check_data(file_path='../data/earthquakes-projected.parq'):
         columns = ['depth', 'id', 'latitude', 'longitude', 'mag', 'place', 'time', 'type']
         data = pd.read_parquet(path, columns=columns, engine='pyarrow')
         data.head()
-        print("Data exists and is readable!")       
+        print("Data exists and is readable!")
     except RuntimeError as e:
         print(f"The data cannot be read: {str(e)}")
