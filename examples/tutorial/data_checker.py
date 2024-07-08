a77b5e6468e90bad614fa92a9da008a8cb4aa80a
Andrew <15331990+ahuang11@users.noreply.github.com>
2024-07-08 16:16:35 -0700

    Update symlink for Scipy shared data directory (#415)
    * Tweak tutorial
    
    * Update examples/tutorial/data_checker.py

--- examples/tutorial/data_checker.py
+++ examples/tutorial/data_checker.py
@@ -14,12 +14,14 @@ def check_data(file_path='../data/earthquakes-projected.parq'):
 
     # Hack to make Nebari installation work for SciPy 2024; can be deleted after the conference
     try:
-        source = "/shared/scipy/hvplot-and-panel"
-        dest = "../data"
-        if os.path.exists(source) and not os.path.exists(dest):
-            print(f"Adding symlink to data files: {str(dest)} -> {str(source)}")
-            os.symlink(source, dest)
-
+        source = pathlib.Path("/shared/scipy/hvplot-and-panel")
+        dest = pathlib.Path("../data")
+        data_path = dest / "earthquakes-projected.parq"
+        if source.exists() and not data_path.exists():
+            for path in source.glob("*"):
+                linked_path = dest / path.name
+                print(f"Adding symlink to data file: {str(path)} -> {str(linked_path)}")
+                linked_path.symlink_to(path, target_is_directory=path.is_dir())
     except RuntimeError as e:
         print(f"Encountered exception during SciPy/Nebari setup: {str(e)}")
 
